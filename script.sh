f56b6ad77da5c1042728fe63244f28bf361be035
vishnuk9 <ranjeetkumar933342@gmail.com>
2023-05-01 13:49:20 +0530

    aa

--- script.sh
+++ script.sh
@@ -7,11 +7,11 @@ echo "FAMILY= " $FAMILY
 NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
 echo "NAME= " $NAME
 
-sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" task-definition.json
-sed -i "s#REPOSITORY_URI#$IMAGE_REPO_NAME#g" task-definition.json
-sed -i "s#ROLE_ARN#$ROLE_ARN#g" task-definition.json
-sed -i "s#FAMILY#$FAMILY#g" task-definition.json
-sed -i "s#NAME#$NAME#g" task-definition.json
+sed -i "s#BUILD_NUMBER#$IMAGE_TAG#g" ./task-definition.json
+sed -i "s#REPOSITORY_URI#$IMAGE_REPO_NAME#g" ./task-definition.json
+sed -i "s#ROLE_ARN#$ROLE_ARN#g" ./task-definition.json
+sed -i "s#FAMILY#$FAMILY#g" ./task-definition.json
+sed -i "s#NAME#$NAME#g" ./task-definition.json
 
 
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
